446238c4d032c327f5da9e60ec3d3e39e3cbdfaf
mehmet <mehmetcimen23@gmail.com>
2019-12-20 14:39:30 +0000

    Using the withRouter function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Homepage} />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./menu-item.styles.scss";
+import { withRouter } from 'react-router-dom';
+
+
 const MenuItem = ({ title, imageUrl, size }) => {
     return (
         <div className={`${size} menu-item`}>
-            <div className="background-image" style={{
-                backgroundImage: `url(${imageUrl})`
-            }} />
+            <div className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subitle">SHOP NOW</span>
@@ -14,4 +19,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
