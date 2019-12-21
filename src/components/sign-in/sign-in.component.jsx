c23abf485cb8b6ffffae8820f0e91e156e1ea0a4
mehmet <mehmetcimen23@gmail.com>
2019-12-21 14:35:45 +0000

    Created a custom, reusable button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 class SignIn extends Component {
     constructor(props) {
         super(props);
@@ -45,9 +46,9 @@ class SignIn extends Component {
                         require="true"
                         handleChange={this.handleChange}
                     />
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit">Sign In </CustomButton>
                 </form>
-            </div>);
+            </div >);
     }
 }
 
