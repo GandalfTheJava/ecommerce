2db694a1591406de03b4bbcdb10b90d53a3a86a1
mehmet <mehmetcimen23@gmail.com>
2019-12-21 12:34:18 +0000

    Set up the base component that will be used to hold the Sign In and Sign up features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
 import './App.css';
+import SignInAndSignUp from "../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
@@ -11,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/Shop" component={ShopPage} />
+        <Route path="/SigIn" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./sign-in-and-sign-up.styles.css";
+
+const SignInAndSignUp = () => {
+    return (
+
+        <div className="sign-in-and-sign-up">SIGN IN
+
+        </div>);
+}
+
+export default SignInAndSignUp;
