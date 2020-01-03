a64300208c4600e8d239c58dce38699df340029e
mehmet <mehmetcimen23@gmail.com>
2020-01-03 14:21:00 +0000

    Additional comments to help undertand the code a little better

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,15 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id //See if new item is already in the cart using the unique ID
     );
-
+    //True/undefined
     if (existingCartItem) {
-        return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd.id
+        return cartItems.map(cartItem => //Return new array
+            cartItem.id === cartItemToAdd.id //If on the same item, increase the quantity, else return the exact item object
                 ? { ...cartItem, quantity: cartItem.quantity + 1 }
                 : cartItem
         );
     }
-
+    //If item does not exist, return same cart with quantity of 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
